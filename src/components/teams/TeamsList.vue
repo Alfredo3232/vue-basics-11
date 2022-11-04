b80ce3cf879346fe67af2ac968e35413027cc5fc
arojas32 <61888719+arojas32@users.noreply.github.com>
2022-11-04 16:24:29 -0600

    tried to set up getting all members in a team

--- src/components/teams/TeamsList.vue
+++ src/components/teams/TeamsList.vue
@@ -1,6 +1,7 @@
 <template>
   <ul>
-    <teams-item v-for="team in teams" :key="team.id" :name="team.name" :member-count="team.members.length"></teams-item>
+    <teams-item v-for="team in teams" :key="team.id" :id="team.id" :name="team.name"
+      :member-count="team.members.length"></teams-item>
   </ul>
 </template>
 
